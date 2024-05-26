5e05c6fd4b8aad8aac50e44ee6ca3b05bea1cc56
Gowthami <gowthamisukhavasi@yahoo.in>
2024-05-25 21:07:18 -0400

    Insertion sort updated

--- DataStructures_Algorithms/InsertionSort/insertionSort.js
+++ DataStructures_Algorithms/InsertionSort/insertionSort.js
@@ -1,5 +1,4 @@
 /*
-
 Implement a function that performs an insertion sort. 
 The function should take in an array of integers and return an array with the integers sorted in ascending order. 
 The input array is modified.
@@ -7,24 +6,32 @@ The input array is modified.
 Examples:
 insertionSort([9, 3, 6, 2, 1, 11]); // [1, 2, 3, 6, 9, 11]
 insertionSort([12, 16, 14, 1, 2, 3]); // [1, 2, 3, 12, 14, 16]
-
 */
 
 function insertionSort(arr) {
   console.log(arr);
   if (arr.length < 2) return arr;
 
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      console.log("i,j,arr[i],arr[j]", i, j, arr[i], arr[j]);
-      if (arr[i] > arr[j]) {
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        console.log("i,j,arr[i],arr[j]", i, j, arr[i], arr[j]);
-      }
+  for (let i = 1; i < arr.length; i++) {
+    let currentEle = arr[i];
+    let lastIndex = i - 1;
+
+    console.log(
+      `i: ${i},arr[i]: ${arr[i]}, currentEle: ${currentEle}, lastIndex: ${lastIndex}`
+    );
+
+    while (lastIndex >= 0 && arr[lastIndex] > currentEle) {
+      console.log(
+        `lastIndex ${lastIndex} , arr[lastIndex]: ${arr[lastIndex]} , currentEle: ${currentEle}`
+      );
+      arr[lastIndex + 1] = arr[lastIndex];
+      lastIndex--;
+      console.log("in while loop, lastIndex, arr", lastIndex, arr);
     }
+    arr[lastIndex + 1] = currentEle;
+    console.log("arr", arr);
   }
+
   return arr;
 }
 
--- DataStructures_Algorithms/InsertionSort/test.js
+++ DataStructures_Algorithms/InsertionSort/test.js
@@ -1,27 +1,27 @@
 const insertionSort = require("./insertionSort");
 
-test("insertionSort is a function", () => {
-  expect(typeof insertionSort).toEqual("function");
-});
+// test("insertionSort is a function", () => {
+//   expect(typeof insertionSort).toEqual("function");
+// });
 
-test("When arr is empty", () => {
-  expect(insertionSort([])).toEqual([]);
-});
+// test("When arr is empty", () => {
+//   expect(insertionSort([])).toEqual([]);
+// });
 
-test("When arr has one element", () => {
-  expect(insertionSort([1])).toEqual([1]);
-});
+// test("When arr has one element", () => {
+//   expect(insertionSort([1])).toEqual([1]);
+// });
 
 test("When arr has two elements", () => {
-  expect(insertionSort([2, 1])).toEqual([1, 2]);
+  expect(insertionSort([50, 30])).toEqual([30, 50]);
   expect(insertionSort([1, 2])).toEqual([1, 2]);
 });
 
 test("When arr has two elements", () => {
-  expect(insertionSort([10, 2, 4])).toEqual([2, 4, 10]);
+  expect(insertionSort([40, 20, 10])).toEqual([10, 20, 40]);
   expect(insertionSort([2, 1, 2])).toEqual([1, 2, 2]);
 });
 
-test("When arr has two elements", () => {
-  expect(insertionSort([9, 3, 6, 2, 1, 11])).toEqual([1, 2, 3, 6, 9, 11]);
-});
+// test("When arr has two elements", () => {
+//   expect(insertionSort([9, 3, 6, 2, 1, 11])).toEqual([1, 2, 3, 6, 9, 11]);
+// });
